eaef2a006b1d35d8cff6e2cb9af654a2a355b8a0
MarcusBruDiaz <marcos.bru@udea.edu.co>
2022-09-22 20:18:28 -0500

    fusion carrito de compras, lista de productus, menu desktop y menu movil

--- main.js
+++ main.js
@@ -1,9 +1,37 @@
 const menuEmail = document.querySelector('.navbar-right-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 
+
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+const carroCompras = document.querySelector('.navbar-right-shopping');
+const menuCompras = document.querySelector('.product-detail');
+
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+carroCompras.addEventListener('click', toggleCarritoMenu);
 
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
+    menuCompras.classList.add('inactive')
+
 }
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
+    menuCompras.classList.add('inactive')
+}
+
+
+function toggleCarritoMenu(){
+    menuCompras.classList.toggle('inactive');
+    mobileMenu.classList.add('inactive');
+}
+
+
+
+const productList =[];
+
+productList
