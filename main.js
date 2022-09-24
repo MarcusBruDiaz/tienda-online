505b31100e4a3c8f47fa9ebc936cd5fa760c3e11
MarcusBruDiaz <marcos.bru@udea.edu.co>
2022-09-23 19:01:52 -0500

    fusionar detalles de productos

--- main.js
+++ main.js
@@ -55,7 +55,8 @@ productList.push({
     precio:620,
     imagen:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
 });
-    
+
+
 
 
 /*<!-- <div class="main-container-cards-product">
